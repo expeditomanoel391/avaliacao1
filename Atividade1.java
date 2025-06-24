163f4e67f708b9a10461de8d89d75e88ae66972b
expeditomanoel391 <expeditomanoel391@gmail.com>
2025-06-24 13:48:05 -0300

    Add files via upload

---
+++ Atividade1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Atividade1 {
+  public static void main(String args[]) {
+Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite sua 1a nota: ");
+    float nota1 = scanner.nextFloat();
+
+    System.out.print("Digite sua 2a nota: ");
+    float nota2 = scanner.nextFloat();
+
+    System.out.print("Digite sua 3a nota: ");
+    float nota3 = scanner.nextFloat();
+
+    System.out.print("Digite sua 4a nota: ");
+    float nota4 = scanner.nextFloat();
+
+    System.out.print("Digite sua 5a nota: ");
+    float nota5 = scanner.nextFloat();
+
+    System.out.print("Digite sua 6a nota: ");
+    float nota6 = scanner.nextFloat();
+
+    System.out.print("Digite sua 7a nota: ");
+    float nota7 = scanner.nextFloat();
+
+    System.out.print("Digite sua 8a nota: ");
+    float nota8 = scanner.nextFloat();
+
+    float bimestre1 = (nota1 + nota2)/2;
+    float bimestre2 = (nota3 + nota4)/2;
+    float bimestre3 = (nota5 + nota6)/2;
+    float bimestre4 = (nota7 + nota8)/2;
+
+
